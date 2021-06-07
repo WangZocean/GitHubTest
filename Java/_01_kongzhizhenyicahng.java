819f31f09a50f83e8d606d1637500050ff7a5302
WangZocean <WangZocean@qq.com>
2021-06-07 20:12:18 +0800

    mian

---
+++ Java/_01_kongzhizhenyicahng.java
@@ -0,0 +1,15 @@
+public class _01_kongzhizhenyicahng {
+    public static void main(String[] args) {
+
+    int[] arr =  new int[3];
+    
+    System.out.println(arr[2]);
+
+    arr = null;
+    //NullPointerException 空指针异常
+    System.out.println(arr[2]);
+
+
+    }
+}
+
